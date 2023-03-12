ba497ec004e25a3703f99b7d95ccd15ead567468
TehCK <70246626+TehCK@users.noreply.github.com>
2023-03-12 15:02:45 +0800

    Delete logOut.php

--- logOut.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Set the cookie expiration time to a past date
-setcookie('user', '', time() - 3600, '/');
-
-// Redirect to the login page
-header('Location: Landing-page-or-Login-page.php');
-exit;
-?>
